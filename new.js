41b0f0a94545e1e70841a7992e3e3ba0d793e8f8
Khushi Gupta <146002143+KHUSHIGUP07@users.noreply.github.com>
2025-06-05 22:33:36 +0530

    Delete new.js

--- new.js
+++
@@ -1,8 +0,0 @@
-/*document.getElementById('contact-form').addEventListener('submit', function(event));
- {
-    event.preventDefault(); // Prevent the default form submission
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message');
-}*/
